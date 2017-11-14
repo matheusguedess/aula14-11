5dc63a3755401ebb1ad7b9f5ed60371a7bde34f6
Matheus Guedes <33043368+matheusguedess@users.noreply.github.com>
2017-11-14 20:09:42 -0200

    Update calculadorafuncao.c

--- calculadorafuncao.c
+++ calculadorafuncao.c
@@ -63,28 +63,20 @@ int main(void)
 
 int soma(int n1, int n2)
 {
-    int result;
-    result=n1+n2;
-    return(result);
+    return(n1+n2);
 }
 
 int sub(int n1, int n2)
 {
-    int result;
-    result=n1-n2;
-    return(result);
+    return(n1-n2);
 }
 
 int mult(int n1, int n2)
 {
-    int result;
-    result=n1*n2;
-    return(result);
+    return(n1*n2);
 }
 
 int div(int n1, int n2)
 {
-    int result;
-    result=n1/n2;
-    return(result);
+    return(n1/n2);
 }
